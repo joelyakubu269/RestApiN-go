f709c1ab32d066bbdc0ef647724caf09f3936ff4
joyakubu <joelyakubu269@gmail.com>
2026-08-20 12:31:48 +0100

    checking out how it works

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
+
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 func main() {
 	server:= gin.Default()
+	server.GET("/events",getEvents)
+	server.Run(":8080")
+}
+func getEvents(context *gin.Context){
+	context.JSON(http.StatusOK,gin.H{"Message": "marhaba ma ismuk?"})
 }
